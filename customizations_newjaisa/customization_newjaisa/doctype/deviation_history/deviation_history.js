a8b9bb1f662d6c207e7123ac19bcfc856d8bb70c
samsparky <samuvel7860@gmail.com>
2023-06-27 03:09:25 +0530

    doc and api commit

---
+++ customizations_newjaisa/customization_newjaisa/doctype/deviation_history/deviation_history.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, samuvel and tech team newjaisa  and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Deviation History', {
+	// refresh: function(frm) {
+
+	// }
+});
